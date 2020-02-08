8ea72fc96e30d6b3258e2b7498f2d25c63246bb6
inderjeet709 <60532163+inderjeet709@users.noreply.github.com>
2020-02-08 22:22:37 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello team Its inderjeet";
+        return "Hello Docker World";
     }
 
     public static void main(String[] args) {
